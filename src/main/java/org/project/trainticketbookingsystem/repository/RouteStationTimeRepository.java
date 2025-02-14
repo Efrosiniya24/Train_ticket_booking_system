263b208818b2110824692d47ad766414d6c9f3de
Efrosiniya24 <efrosiniya24@gmail.com>
2025-02-14 16:10:19 +0300

    repository for the station in the route

---
+++ src/main/java/org/project/trainticketbookingsystem/repository/RouteStationTimeRepository.java
@@ -0,0 +1,9 @@
+package org.project.trainticketbookingsystem.repository;
+
+import org.project.trainticketbookingsystem.entity.RouteStationTimeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RouteStationTimeRepository extends JpaRepository<RouteStationTimeEntity, Long> {
+}
