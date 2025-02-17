02594001ad66b39cbad2c719cd80604defbd9714
Efrosiniya24 <efrosiniya24@gmail.com>
2025-02-17 16:57:53 +0300

    create seat

---
+++ src/main/java/org/project/trainticketbookingsystem/service/impl/SeatServiceImpl.java
@@ -0,0 +1,32 @@
+package org.project.trainticketbookingsystem.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.project.trainticketbookingsystem.dto.SeatDTO;
+import org.project.trainticketbookingsystem.entity.CoachEntity;
+import org.project.trainticketbookingsystem.entity.SeatEntity;
+import org.project.trainticketbookingsystem.mapper.SeatMapper;
+import org.project.trainticketbookingsystem.repository.SeatRepository;
+import org.project.trainticketbookingsystem.service.SeatService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SeatServiceImpl implements SeatService {
+    private final SeatRepository seatRepository;
+    private final SeatMapper seatMapper;
+
+    @Override
+    public List<SeatDTO> createSeat(CoachEntity coachEntity, List<SeatDTO> seatDTOs) {
+        List<SeatEntity> seatEntities = seatDTOs.stream()
+                .map(seatDTO -> SeatEntity.builder()
+                        .price(seatDTO.getPrice())
+                        .coach(coachEntity)
+                        .build())
+                .toList();
+
+        seatRepository.saveAll(seatEntities);
+        return seatMapper.toSeatDTO(seatEntities);
+    }
+}
