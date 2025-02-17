5164bb7983c02a5966574cef0d651e8fc79440aa
Efrosiniya24 <efrosiniya24@gmail.com>
2025-02-17 16:54:54 +0300

    add train

--- src/main/java/org/project/trainticketbookingsystem/service/impl/TrainServiceImpl.java
+++ src/main/java/org/project/trainticketbookingsystem/service/impl/TrainServiceImpl.java
@@ -1,12 +1,15 @@
 package org.project.trainticketbookingsystem.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.project.trainticketbookingsystem.dto.CoachDTO;
 import org.project.trainticketbookingsystem.dto.TrainDTO;
 import org.project.trainticketbookingsystem.entity.TrainEntity;
 import org.project.trainticketbookingsystem.mapper.TrainMapper;
 import org.project.trainticketbookingsystem.repository.TrainRepository;
+import org.project.trainticketbookingsystem.service.CoachService;
 import org.project.trainticketbookingsystem.service.TrainService;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -15,10 +18,21 @@ public class TrainServiceImpl implements TrainService {
 
     private final TrainRepository trainRepository;
     private final TrainMapper trainMapper;
+    private final CoachService coachService;
 
     @Override
     public TrainDTO addTrain(TrainDTO train) {
-        return null;
+        TrainEntity trainEntity = new TrainEntity();
+        trainEntity.setTrain(train.getTrain());
+        trainRepository.save(trainEntity);
+
+        List<CoachDTO> coachDTOS = train.getCoachDTOList();
+
+        for (CoachDTO coachDTO : coachDTOS) {
+            coachService.createCoach(trainEntity, coachDTO);
+        }
+
+        return trainMapper.toTrainDTO(trainEntity);
     }
 
     @Override
