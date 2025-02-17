11988600b6ac549203039b250beea60e13908340
Efrosiniya24 <efrosiniya24@gmail.com>
2025-02-17 16:58:58 +0300

    seat dto

---
+++ src/main/java/org/project/trainticketbookingsystem/dto/SeatDTO.java
@@ -0,0 +1,10 @@
+package org.project.trainticketbookingsystem.dto;
+
+import lombok.Data;
+
+@Data
+public class SeatDTO {
+    private Long id;
+    private int number;
+    private double price;
+}
