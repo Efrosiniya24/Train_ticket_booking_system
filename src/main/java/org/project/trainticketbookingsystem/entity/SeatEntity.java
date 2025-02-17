be30e5f3bbd4ba391f3d8ab043cfc538b140d26c
Efrosiniya24 <efrosiniya24@gmail.com>
2025-02-17 16:56:30 +0300

    add Builder

--- src/main/java/org/project/trainticketbookingsystem/entity/SeatEntity.java
+++ src/main/java/org/project/trainticketbookingsystem/entity/SeatEntity.java
@@ -1,10 +1,7 @@
 package org.project.trainticketbookingsystem.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
@@ -14,14 +11,11 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class SeatEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "train_id")
-    private TrainEntity train;
 
     private double price;
 
