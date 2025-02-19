3befea0cca29cc842798e8e363fb2cc144548d47
Efrosiniya24 <efrosiniya24@gmail.com>
2025-02-19 11:34:18 +0300

    findByName

--- src/main/java/org/project/trainticketbookingsystem/repository/StationRepository.java
+++ src/main/java/org/project/trainticketbookingsystem/repository/StationRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StationRepository extends JpaRepository<StationEntity, Long> {
+    StationEntity findByName(String name);
 }
