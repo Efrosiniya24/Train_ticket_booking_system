70c3eca003fc34c29bfceb930f2dae8fd5a8bf27
Efrosiniya24 <efrosiniya24@gmail.com>
2025-02-19 11:34:38 +0300

    to stationEntity mapper

--- src/main/java/org/project/trainticketbookingsystem/mapper/StationMapper.java
+++ src/main/java/org/project/trainticketbookingsystem/mapper/StationMapper.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface StationMapper {
     List<StationDTO> toStationDTO(List<StationEntity> stationEntities);
+    StationEntity toStationEntity(StationDTO stationDTO);
 }
