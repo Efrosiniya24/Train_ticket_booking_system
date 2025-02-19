7d87daae51db6bd95d3eaee46f8ab7c615a46e6b
Efrosiniya24 <efrosiniya24@gmail.com>
2025-02-19 23:38:36 +0300

    builder annotation

--- src/main/java/org/project/trainticketbookingsystem/dto/RouteDTO.java
+++ src/main/java/org/project/trainticketbookingsystem/dto/RouteDTO.java
@@ -1,6 +1,7 @@
 package org.project.trainticketbookingsystem.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class RouteDTO {
     private Long trainId;
     private List<RouteStationTimeDTO> routeStationTimeDTO;
