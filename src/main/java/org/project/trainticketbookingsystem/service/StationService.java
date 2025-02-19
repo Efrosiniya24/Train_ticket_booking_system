a1f4375720e96d9568eb818e6ce8e7b7ca5e703d
Efrosiniya24 <efrosiniya24@gmail.com>
2025-02-19 11:35:07 +0300

    get stationByName

--- src/main/java/org/project/trainticketbookingsystem/service/StationService.java
+++ src/main/java/org/project/trainticketbookingsystem/service/StationService.java
@@ -1,13 +1,13 @@
 package org.project.trainticketbookingsystem.service;
 
 import org.project.trainticketbookingsystem.dto.StationDTO;
+import org.project.trainticketbookingsystem.entity.StationEntity;
 
 import java.util.List;
 
 public interface StationService {
     List<StationDTO> getAllStations();
-
     void addStation(StationDTO stationDTO);
-
     void deleteStation(Long id);
+    StationEntity getStationByName(String stationName);
 }
--- src/main/java/org/project/trainticketbookingsystem/service/impl/StationServiceImpl.java
+++ src/main/java/org/project/trainticketbookingsystem/service/impl/StationServiceImpl.java
@@ -7,6 +7,7 @@ import org.project.trainticketbookingsystem.mapper.StationMapper;
 import org.project.trainticketbookingsystem.repository.StationRepository;
 import org.project.trainticketbookingsystem.service.StationService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -32,5 +33,11 @@ public class StationServiceImpl implements StationService {
     @Override
     public void deleteStation(Long id) {
         stationRepository.deleteById(id);
+    }
+
+    @Transactional
+    @Override
+    public StationEntity getStationByName(String stationName) {
+        return stationRepository.findByName(stationName);
     }
 }
