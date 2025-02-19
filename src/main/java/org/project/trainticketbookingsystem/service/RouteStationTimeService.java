3705fdfe37e26b7fe3d7f13539fb93d913699f1e
Efrosiniya24 <efrosiniya24@gmail.com>
2025-02-19 11:32:15 +0300

    create routeStationTime

---
+++ src/main/java/org/project/trainticketbookingsystem/service/RouteStationTimeService.java
@@ -0,0 +1,11 @@
+package org.project.trainticketbookingsystem.service;
+
+import org.project.trainticketbookingsystem.dto.RouteStationTimeDTO;
+import org.project.trainticketbookingsystem.entity.RouteEntity;
+import org.project.trainticketbookingsystem.entity.RouteStationTimeEntity;
+
+import java.util.List;
+
+public interface RouteStationTimeService {
+    List<RouteStationTimeEntity> create(List<RouteStationTimeDTO> routeStationTimeDTOS, RouteEntity routeEntity);
+}
--- src/main/java/org/project/trainticketbookingsystem/service/impl/RouteStationTimeServiceImpl.java
+++ src/main/java/org/project/trainticketbookingsystem/service/impl/RouteStationTimeServiceImpl.java
@@ -4,25 +4,39 @@ import lombok.RequiredArgsConstructor;
 import org.project.trainticketbookingsystem.dto.RouteStationTimeDTO;
 import org.project.trainticketbookingsystem.entity.RouteEntity;
 import org.project.trainticketbookingsystem.entity.RouteStationTimeEntity;
+import org.project.trainticketbookingsystem.entity.StationEntity;
 import org.project.trainticketbookingsystem.mapper.RouteStationTimeMapper;
+import org.project.trainticketbookingsystem.repository.RouteRepository;
 import org.project.trainticketbookingsystem.repository.RouteStationTimeRepository;
+import org.project.trainticketbookingsystem.service.RouteStationTimeService;
+import org.project.trainticketbookingsystem.service.StationService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class RouteStationTimeServiceImpl {
+public class RouteStationTimeServiceImpl implements RouteStationTimeService {
     private final RouteStationTimeRepository routeStationTimeRepository;
     private final RouteStationTimeMapper routeStationTimeMapper;
+    private final StationService stationService;
 
+    @Override
     public List<RouteStationTimeEntity> create(List<RouteStationTimeDTO> routeStationTimeDTO, RouteEntity routeEntity) {
-        List<RouteStationTimeEntity> routeStationTime = routeStationTimeMapper.toRouteStationTimeEntity(routeStationTimeDTO);
+        List<RouteStationTimeEntity> routeStationTimes = routeStationTimeDTO.stream()
+                .map(dto -> {
+                    RouteStationTimeEntity entity = routeStationTimeMapper.toRouteStationTimeEntity(dto);
+                    entity.setRoute(routeEntity);
 
-        routeStationTime
-                .forEach(routeStation -> routeStation.setRoute(routeEntity));
+                    StationEntity station = stationService.getStationByName(dto.getStationDTO().getName());
+                    entity.setStation(station);
 
-        routeStationTimeRepository.saveAll(routeStationTime);
-        return routeStationTime;
+                    return entity;
+                })
+                .collect(Collectors.toList());
+
+            routeStationTimeRepository.saveAll(routeStationTimes);
+        return routeStationTimes;
     }
 }
