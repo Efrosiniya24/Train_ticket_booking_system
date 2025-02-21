f408e4bf19d88729692ef1ea216a4c8d8b0392cb
Efrosiniya24 <efrosiniya24@gmail.com>
2025-02-21 10:58:22 +0300

    builder annotation, get password

--- src/main/java/org/project/trainticketbookingsystem/entity/UserEntity.java
+++ src/main/java/org/project/trainticketbookingsystem/entity/UserEntity.java
@@ -1,10 +1,7 @@
 package org.project.trainticketbookingsystem.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -17,7 +14,8 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserEntity implements UserDetails {
+@Builder
+public class UserEntity implements UserDetails{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -60,5 +58,9 @@ public class UserEntity implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+    @Override
+    public String getPassword() {
+        return password;
     }
 }
