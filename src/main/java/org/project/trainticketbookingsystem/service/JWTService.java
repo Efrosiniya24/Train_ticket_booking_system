c4186e7cb82e15e3553bf1fbd55a23eff3e33f9c
Efrosiniya24 <efrosiniya24@gmail.com>
2025-02-21 11:46:28 +0300

    extractClaim, extractAllClaims, extractUsername, isTokenValid

--- src/main/java/org/project/trainticketbookingsystem/service/JWTService.java
+++ src/main/java/org/project/trainticketbookingsystem/service/JWTService.java
@@ -1,7 +1,15 @@
 package org.project.trainticketbookingsystem.service;
 
+import io.jsonwebtoken.Claims;
 import org.project.trainticketbookingsystem.entity.UserEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.function.Function;
 
 public interface JWTService {
     String generateAccessToken(UserEntity user);
+    <T> T extractClaim(String token, Function<Claims, T> claimsResolver);
+    Claims extractAllClaims(String token);
+    String extractUsername(String jwt);
+    boolean isTokenValid(String token, UserDetails userDetails);
 }
--- src/main/java/org/project/trainticketbookingsystem/service/impl/JWTServiceImpl.java
+++ src/main/java/org/project/trainticketbookingsystem/service/impl/JWTServiceImpl.java
@@ -1,5 +1,7 @@
 package org.project.trainticketbookingsystem.service.impl;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -7,10 +9,12 @@ import lombok.RequiredArgsConstructor;
 import org.project.trainticketbookingsystem.entity.UserEntity;
 import org.project.trainticketbookingsystem.service.JWTService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
@@ -29,10 +33,43 @@ public class JWTServiceImpl implements JWTService {
                 .compact();
     }
 
+    @Override
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
     private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
+    @Override
+    public Claims extractAllClaims(String token) throws JwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
 
+    @Override
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    @Override
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
 }
