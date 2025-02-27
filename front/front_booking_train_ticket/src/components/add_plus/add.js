5ef8a6ff8cdf9f80386eb1697cfd663ac08e8b23
Efrosiniya24 <efrosiniya24@gmail.com>
2025-02-28 00:26:06 +0300

    add icon

---
+++ front/front_booking_train_ticket/src/components/add_plus/add.js
@@ -0,0 +1,11 @@
+import style from "./add.module.css"
+
+const Plus = () => {
+    return ( 
+        <div className={style.plus}>
+            <p>+</p>
+        </div>
+    );
+}
+ 
+export default Plus;
