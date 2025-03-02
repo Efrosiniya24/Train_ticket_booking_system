4cf5a3216ffc4aac08e7297ba056d5238e6ae52b
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-02 13:05:26 +0300

    train header

--- front/front_booking_train_ticket/src/components/sideBar/sideBar.js
+++ front/front_booking_train_ticket/src/components/sideBar/sideBar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import style from "./sideBar.module.css";
 import route from "./icons/route.png";
+import train from "./icons/train.png"
 
 
 const SideBarAdmin = () => {
@@ -12,6 +13,12 @@ const SideBarAdmin = () => {
                     <NavLink to="/route" activeClassName="active" className={style.menuLine}>
                         <img src={route} alt="Route icon" className={style.menuIcon}/>
                         <p className={style.menu}>Марштуры</p>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/train" activeClassName="active" className={style.menuLine}>
+                        <img src={train} alt="Train icon" className={style.menuIcon}/>
+                        <p className={style.menu}>Поезда</p>
                     </NavLink>
                 </li>
             </ul>
