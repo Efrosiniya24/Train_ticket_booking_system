9ea70d5c3272064cffab6a8452ab7eb27eb39ee7
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-02 14:24:51 +0300

    train route

--- front/front_booking_train_ticket/src/App.js
+++ front/front_booking_train_ticket/src/App.js
@@ -5,6 +5,7 @@ import SignIn from './pages/authorization/SignIn';
 import SignUp from './pages/authorization/SignUp';
 import RoutePage from './pages/route/route';
 import { AuthProvider } from './components/AuthProvider';
+import Train from './pages/train/train';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/signIn" element={<SignIn/>}></Route>
           <Route path="/signUp" element={<SignUp/>}></Route>
           <Route path="/route" element={<RoutePage/>}></Route>
+          <Route path="/train" element={<Train/>}></Route>
         </Routes>
       </Router>
     </AuthProvider>
