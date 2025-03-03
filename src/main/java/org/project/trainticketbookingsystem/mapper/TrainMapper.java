4c0757e54565a202515f9b41ff6b66ab7e03735f
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-03 12:29:59 +0300

    to train entity

--- src/main/java/org/project/trainticketbookingsystem/mapper/TrainMapper.java
+++ src/main/java/org/project/trainticketbookingsystem/mapper/TrainMapper.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface TrainMapper {
     List<TrainDTO> toTrainDTO(List<TrainEntity> trainEntity);
     TrainDTO toTrainDTO(TrainEntity trainEntity);
+    TrainEntity toTrainEntity(TrainDTO trainDTO);
 }
