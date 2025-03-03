37882a9473e4402c97f969421b665fa7d42d0bd8
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-03 12:30:47 +0300

    builder annotation

--- src/main/java/org/project/trainticketbookingsystem/dto/TrainDTO.java
+++ src/main/java/org/project/trainticketbookingsystem/dto/TrainDTO.java
@@ -1,9 +1,11 @@
 package org.project.trainticketbookingsystem.dto;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Builder
 public class TrainDTO {
     private Long id;
     private String train;
