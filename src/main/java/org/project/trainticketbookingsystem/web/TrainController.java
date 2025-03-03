aa458c263129a8b064c4f2cf606fceb81880a101
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-03 09:00:36 +0300

    add exception

--- src/main/java/org/project/trainticketbookingsystem/web/TrainController.java
+++ src/main/java/org/project/trainticketbookingsystem/web/TrainController.java
@@ -3,6 +3,7 @@ package org.project.trainticketbookingsystem.web;
 import lombok.AllArgsConstructor;
 import org.project.trainticketbookingsystem.dto.TrainDTO;
 import org.project.trainticketbookingsystem.service.TrainService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -19,8 +20,13 @@ public class TrainController {
     @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping("/addTrain")
     public ResponseEntity<TrainDTO> addTrain(@RequestBody TrainDTO trainDTO) {
-        TrainDTO train = trainService.addTrain(trainDTO);
-        return ResponseEntity.ok(train);
+        TrainDTO train;
+        try {
+              train= trainService.addTrain(trainDTO);
+            return ResponseEntity.ok(train);
+        }catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.FOUND).body(null);
+        }
     }
 
     @GetMapping("/allTrains")
@@ -31,14 +37,19 @@ public class TrainController {
 
     @GetMapping("/numberOfSeats/{id}")
     public ResponseEntity<Integer> getNumberOfSeats(@PathVariable Long id) {
-        int numberOfSeats = trainService.getNumberOfSeats(id);
-        return ResponseEntity.ok(numberOfSeats);
+        int numberOfSeats;
+        try {
+            numberOfSeats = trainService.getNumberOfSeats(id);
+            return ResponseEntity.ok(numberOfSeats);
+        }catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
+
     @PreAuthorize("hasAuthority('ADMIN')")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteTrain(@PathVariable Long id) {
         trainService.deleteTrainById(id);
         return ResponseEntity.ok().body(null);
     }
-
 }
