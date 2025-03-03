f867d41590358b977ce332457e29c10fe0f2ff02
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-03 12:31:03 +0300

    findUserByEmail

--- src/main/java/org/project/trainticketbookingsystem/service/UserService.java
+++ src/main/java/org/project/trainticketbookingsystem/service/UserService.java
@@ -1,2 +1,7 @@
-package org.project.trainticketbookingsystem.service;public class UserService {
+package org.project.trainticketbookingsystem.service;
+
+import org.project.trainticketbookingsystem.entity.UserEntity;
+
+public interface UserService {
+    UserEntity findUserByEmail(String email);
 }
