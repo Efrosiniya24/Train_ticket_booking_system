df3e80033a2afa460d6e16dcd42bc1bbb2f9d6f5
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-06 11:23:56 +0300

    adding get price

--- src/main/java/org/project/trainticketbookingsystem/service/SeatService.java
+++ src/main/java/org/project/trainticketbookingsystem/service/SeatService.java
@@ -1,16 +1,17 @@
 package org.project.trainticketbookingsystem.service;
 
-import org.project.trainticketbookingsystem.dto.BookingDTO;
-import org.project.trainticketbookingsystem.dto.SeatDTO;
+import org.project.trainticketbookingsystem.dto.BookingDto;
+import org.project.trainticketbookingsystem.dto.SeatDto;
 import org.project.trainticketbookingsystem.entity.CoachEntity;
 import org.project.trainticketbookingsystem.entity.SeatEntity;
 
 import java.util.List;
 
 public interface SeatService {
-    List<SeatDTO> createSeat(CoachEntity coachEntity, List<SeatDTO> seatDTOs);
-    List<SeatEntity> getSeatEntityFromBooking(BookingDTO bookingDTO);
+    List<SeatDto> createSeat(CoachEntity coachEntity, List<SeatDto> seatDtos);
+    List<SeatEntity> getSeatEntityFromBooking(BookingDto bookingDTO);
     List<Long> getSeatsId(List<SeatEntity> seatEntities);
     void deleteSeat(List<SeatEntity> seatEntity);
-    List<SeatEntity> updateSeats(List<SeatDTO> seats);
+    List<SeatEntity> updateSeats(List<SeatDto> seats);
+    double getPrice(Long trainId);
 }
