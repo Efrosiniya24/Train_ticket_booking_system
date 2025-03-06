92b34c1abb98d7fcf1d2b0dbe1604b63318b6d60
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-06 11:27:36 +0300

    renamed enum

--- src/main/java/org/project/trainticketbookingsystem/entity/UserEntity.java
+++ src/main/java/org/project/trainticketbookingsystem/entity/UserEntity.java
@@ -2,7 +2,7 @@ package org.project.trainticketbookingsystem.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.project.trainticketbookingsystem.Enum.Role;
+import org.project.trainticketbookingsystem.enums.Role;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
