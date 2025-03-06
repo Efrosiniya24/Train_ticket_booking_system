aee52b052810edae1804569aad2b63cea2bc79fa
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-06 16:32:33 +0300

    find By TrainId And RouteId And ArrivalDate

--- src/main/java/org/project/trainticketbookingsystem/repository/BookingRepository.java
+++ src/main/java/org/project/trainticketbookingsystem/repository/BookingRepository.java
@@ -1,30 +1,13 @@
 package org.project.trainticketbookingsystem.repository;
 
-import org.project.trainticketbookingsystem.entity.BookingEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
 import java.time.LocalDate;
 import java.util.List;
+import org.project.trainticketbookingsystem.entity.BookingEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookingRepository extends JpaRepository<BookingEntity, Long> {
-    @Query("SELECT sb FROM BookingEntity sb " +
-            "JOIN sb.seats s " +
-            "JOIN sb.route.routeStationTime rst " +
-            "JOIN rst.station st " +
-            "WHERE s.id IN :seatIds " +
-            "AND sb.train.id = :trainId " +
-            "AND sb.arrivalDate = :travelDate " +
-            "AND (rst.stopOrder < :arrivalStopOrder AND rst.stopOrder > :departureStopOrder)")
-    List<BookingEntity> findConflictingBookings(
-            @Param("seatIds") List<Long> seatIds,
-            @Param("trainId") Long trainId,
-            @Param("travelDate") LocalDate travelDate,
-            @Param("departureStopOrder") int departureStopOrder,
-            @Param("arrivalStopOrder") int arrivalStopOrder
-    );
+    List<BookingEntity> findByTrainIdAndRouteIdAndArrivalDate(Long trainId, Long routeId, LocalDate arrivalDate);
 }
 
