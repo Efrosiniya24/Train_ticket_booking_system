9b235729836c4542898356301cac29917e75d74c
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-06 11:26:24 +0300

    renamed DTO, train exception

--- src/main/java/org/project/trainticketbookingsystem/service/impl/TrainServiceImpl.java
+++ src/main/java/org/project/trainticketbookingsystem/service/impl/TrainServiceImpl.java
@@ -1,10 +1,10 @@
 package org.project.trainticketbookingsystem.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.project.trainticketbookingsystem.dto.CoachDTO;
-import org.project.trainticketbookingsystem.dto.TrainDTO;
-import org.project.trainticketbookingsystem.entity.CoachEntity;
+import org.project.trainticketbookingsystem.dto.CoachDto;
+import org.project.trainticketbookingsystem.dto.TrainDto;
 import org.project.trainticketbookingsystem.entity.TrainEntity;
+import org.project.trainticketbookingsystem.exceptions.TrainException;
 import org.project.trainticketbookingsystem.mapper.TrainMapper;
 import org.project.trainticketbookingsystem.repository.TrainRepository;
 import org.project.trainticketbookingsystem.service.CoachService;
@@ -23,28 +23,28 @@ public class TrainServiceImpl implements TrainService {
     private final CoachService coachService;
 
     @Override
-    public TrainDTO addTrain(TrainDTO train) {
+    public TrainDto addTrain(TrainDto train) {
         TrainEntity trainEntity = trainMapper.toTrainEntity(train);
         if (isExistTrain(trainEntity))
-            throw new RuntimeException("Train is already exist");
+            throw new TrainException("Train is already exist");
 
         trainRepository.save(trainEntity);
 
-        List<CoachDTO> coachDTOS = train.getCoachDTOList();
+        List<CoachDto> coachDtos = train.getCoachDtoList();
 
-        for (CoachDTO coachDTO : coachDTOS) {
+        for (CoachDto coachDTO : coachDtos) {
             coachService.createCoach(trainEntity, coachDTO);
         }
         return trainMapper.toTrainDTO(trainEntity);
     }
 
     @Override
-    public List<TrainDTO> getAllTrains() {//структурав json
+    public List<TrainDto> getAllTrains() {
         List<TrainEntity> trainEntities = trainRepository.findAll();
         return trainEntities.stream()
                 .map(trainEntity -> {
-                    TrainDTO trainDTO = trainMapper.toTrainDTO(trainEntity);
-                    trainDTO.setCoachDTOList(trainEntity.getCoachEntities().stream()
+                    TrainDto trainDTO = trainMapper.toTrainDTO(trainEntity);
+                    trainDTO.setCoachDtoList(trainEntity.getCoachEntities().stream()
                             .map(coachService::toCoachDTO).toList());
                     return trainDTO;
                 })
@@ -52,14 +52,14 @@ public class TrainServiceImpl implements TrainService {
     }
 
     @Override
-    public TrainDTO getTrainById(Long id) {
-        TrainEntity trainEntity = trainRepository.findById(id).orElseThrow(() -> new RuntimeException("Train not found"));
+    public TrainDto getTrainById(Long id) {
+        TrainEntity trainEntity = trainRepository.findById(id).orElseThrow(() -> new TrainException("Train not found"));
         return trainMapper.toTrainDTO(trainEntity);
     }
 
     @Override
     public int getNumberOfSeats(Long trainId) {
-        TrainEntity trainEntity = trainRepository.findById(trainId).orElseThrow(() -> new RuntimeException("Train not found"));
+        TrainEntity trainEntity = trainRepository.findById(trainId).orElseThrow(() -> new TrainException("Train not found"));
         return trainEntity.getCoachEntities().stream()
                 .mapToInt(coach -> coach.getSeats().size())
                 .sum();
@@ -67,24 +67,24 @@ public class TrainServiceImpl implements TrainService {
 
     @Override
     public TrainEntity getTrainEntityById(Long id) {
-        return trainRepository.findById(id).orElseThrow(() -> new RuntimeException("Train not found"));
+        return trainRepository.findById(id).orElseThrow(() -> new TrainException("Train not found"));
     }
 
     @Override
     public void deleteTrainById(Long id) {
         TrainEntity train = trainRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Train not found"));
+                .orElseThrow(() -> new TrainException("Train not found"));
         coachService.deleteCoach(train.getCoachEntities());
         trainRepository.delete(train);
     }
 
     @Override
-    public TrainDTO updateTrain(Long id, TrainDTO trainDTO) {
-        TrainEntity trainEntity = trainRepository.findById(id).orElseThrow(() -> new RuntimeException("Train not found"));
+    public TrainDto updateTrain(Long id, TrainDto trainDTO) {
+        TrainEntity trainEntity = trainRepository.findById(id).orElseThrow(() -> new TrainException("Train not found"));
 
         trainEntity.setId(id);
         trainEntity.setTrain(trainDTO.getTrain());
-        trainEntity.setCoachEntities(coachService.update(trainDTO.getCoachDTOList()));
+        trainEntity.setCoachEntities(coachService.update(trainDTO.getCoachDtoList()));
 
         trainRepository.save(trainEntity);
         return trainMapper.toTrainDTO(trainEntity);
