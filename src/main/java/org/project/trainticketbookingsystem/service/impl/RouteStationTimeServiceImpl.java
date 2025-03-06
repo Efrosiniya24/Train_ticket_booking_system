93c7d83b2b22bfbebe2cec1f28c7850580b6b780
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-06 11:32:52 +0300

    renamed dto, update creation

--- src/main/java/org/project/trainticketbookingsystem/service/impl/RouteStationTimeServiceImpl.java
+++ src/main/java/org/project/trainticketbookingsystem/service/impl/RouteStationTimeServiceImpl.java
@@ -1,19 +1,18 @@
 package org.project.trainticketbookingsystem.service.impl;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.project.trainticketbookingsystem.dto.RouteStationTimeDTO;
+import org.project.trainticketbookingsystem.dto.RouteStationTimeDto;
 import org.project.trainticketbookingsystem.entity.RouteEntity;
 import org.project.trainticketbookingsystem.entity.RouteStationTimeEntity;
 import org.project.trainticketbookingsystem.entity.StationEntity;
+import org.project.trainticketbookingsystem.exceptions.RouteStationTimeException;
 import org.project.trainticketbookingsystem.mapper.RouteStationTimeMapper;
-import org.project.trainticketbookingsystem.mapper.StationMapper;
 import org.project.trainticketbookingsystem.repository.RouteStationTimeRepository;
 import org.project.trainticketbookingsystem.service.RouteStationTimeService;
 import org.project.trainticketbookingsystem.service.StationService;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,21 +22,16 @@ public class RouteStationTimeServiceImpl implements RouteStationTimeService {
     private final StationService stationService;
 
     @Override
-    public List<RouteStationTimeEntity> create(List<RouteStationTimeDTO> routeStationTimeDTO, RouteEntity routeEntity) {
-        List<RouteStationTimeEntity> routeStationTimes = routeStationTimeDTO.stream()
-                .map(dto -> {
-                    RouteStationTimeEntity entity = routeStationTimeMapper.toRouteStationTimeEntity(dto);
-                    entity.setRoute(routeEntity);
+    public List<RouteStationTimeEntity> create(List<RouteStationTimeDto> routeStationTimeDTO, RouteEntity routeEntity) {
+        List<RouteStationTimeEntity> routeStationTimeEntities = routeStationTimeMapper.toRouteStationTimeEntityList(routeStationTimeDTO);
 
-                    StationEntity station = stationService.getStationByName(dto.getStationDTO().getName());
-                    entity.setStation(station);
+        routeStationTimeEntities.forEach(route -> {
+            route.setRoute(routeEntity);
+            StationEntity station = stationService.getStationByName(route.getStation().getName());
+            route.setStation(station);
+        });
 
-                    return entity;
-                })
-                .collect(Collectors.toList());
-
-            routeStationTimeRepository.saveAll(routeStationTimes);
-        return routeStationTimes;
+        return routeStationTimeRepository.saveAll(routeStationTimeEntities);
     }
 
     @Override
@@ -46,10 +40,10 @@ public class RouteStationTimeServiceImpl implements RouteStationTimeService {
     }
 
     @Override
-    public List<RouteStationTimeEntity> update(List<RouteStationTimeDTO> routeStationTimeDTOs, RouteEntity routeEntity) {
-        List<RouteStationTimeEntity> updatedStations = routeStationTimeDTOs.stream()
+    public List<RouteStationTimeEntity> update(List<RouteStationTimeDto> routeStationTimeDtos, RouteEntity routeEntity) {
+        List<RouteStationTimeEntity> updatedStations = routeStationTimeDtos.stream()
                 .map(dto -> {
-                    RouteStationTimeEntity entity = routeStationTimeRepository.findById(dto.getId()).orElseThrow(() -> new RuntimeException("not found"));
+                    RouteStationTimeEntity entity = routeStationTimeRepository.findById(dto.getId()).orElseThrow(() -> new RouteStationTimeException(" Route not found"));
                     entity.setStopOrder(dto.getStopOrder());
                     entity.setDepartureDate(dto.getDepartureDate());
                     entity.setArrivalDate(dto.getArrivalDate());
