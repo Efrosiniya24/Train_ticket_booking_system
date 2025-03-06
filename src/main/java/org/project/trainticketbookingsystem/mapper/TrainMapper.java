04e727cccefc3e1064d26be6befbedbc13b85972
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-06 11:27:55 +0300

    renamed dto

--- src/main/java/org/project/trainticketbookingsystem/mapper/TrainMapper.java
+++ src/main/java/org/project/trainticketbookingsystem/mapper/TrainMapper.java
@@ -1,14 +1,14 @@
 package org.project.trainticketbookingsystem.mapper;
 
 import org.mapstruct.Mapper;
-import org.project.trainticketbookingsystem.dto.TrainDTO;
+import org.project.trainticketbookingsystem.dto.TrainDto;
 import org.project.trainticketbookingsystem.entity.TrainEntity;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TrainMapper {
-    List<TrainDTO> toTrainDTO(List<TrainEntity> trainEntity);
-    TrainDTO toTrainDTO(TrainEntity trainEntity);
-    TrainEntity toTrainEntity(TrainDTO trainDTO);
+    List<TrainDto> toTrainDTO(List<TrainEntity> trainEntity);
+    TrainDto toTrainDTO(TrainEntity trainEntity);
+    TrainEntity toTrainEntity(TrainDto trainDTO);
 }
