6a730a884f3c739da026de469cb67970890c5844
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-06 11:35:00 +0300

    toRouteDTOList, toRouteDTO

--- src/main/java/org/project/trainticketbookingsystem/mapper/RouteMapper.java
+++ src/main/java/org/project/trainticketbookingsystem/mapper/RouteMapper.java
@@ -1,13 +1,31 @@
 package org.project.trainticketbookingsystem.mapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import org.mapstruct.Mapper;
-import org.project.trainticketbookingsystem.dto.RouteDTO;
+import org.mapstruct.Mapping;
+import org.project.trainticketbookingsystem.dto.RouteDto;
+import org.project.trainticketbookingsystem.dto.RouteStationTimeDto;
+import org.project.trainticketbookingsystem.dto.TrainDto;
 import org.project.trainticketbookingsystem.entity.RouteEntity;
 
-import java.util.List;
-
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = RouteStationTimeMapper.class)
 public interface RouteMapper {
-    List<RouteDTO> toRouteDTO(List<RouteEntity> routeEntities);
-    RouteDTO toRouteDTO(RouteEntity routeEntity);
+
+    @Mapping(target = "routeStationTimeDTO", source = "routeStationTime")
+    RouteDto toRouteDTO(RouteEntity routeEntity);
+
+    default List<RouteDto> toRouteDTOList(List<RouteEntity> routeEntities) {
+        return routeEntities.stream()
+                .map(this::toRouteDTO)
+                .collect(Collectors.toList());
+    }
+
+    default RouteDto toRouteDTO(Long routeId, TrainDto trainDto, List<RouteStationTimeDto> routeStationTimeDtos) {
+        return RouteDto.builder()
+                .id(routeId)
+                .train(trainDto)
+                .routeStationTimeDTO(routeStationTimeDtos)
+                .build();
+    }
 }
