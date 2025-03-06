cec43d1a83cf9c4d4d95c963edd48d03a28bdbda
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-06 11:24:12 +0300

    find price

--- src/main/java/org/project/trainticketbookingsystem/repository/SeatRepository.java
+++ src/main/java/org/project/trainticketbookingsystem/repository/SeatRepository.java
@@ -1,9 +1,16 @@
 package org.project.trainticketbookingsystem.repository;
 
+import java.util.List;
+import java.util.Optional;
 import org.project.trainticketbookingsystem.entity.SeatEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SeatRepository extends JpaRepository<SeatEntity, Long> {
+    @Query("SELECT s.price FROM SeatEntity s WHERE s.coach.train.id = :trainId ORDER BY s.id LIMIT 1")
+    double findSinglePriceByTrainId(@Param("trainId") Long trainId);
+
 }
