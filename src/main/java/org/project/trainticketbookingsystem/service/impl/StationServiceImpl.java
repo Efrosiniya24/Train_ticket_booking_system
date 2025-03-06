7d87d843095fda420514cad721e2a5df7da90623
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-06 11:29:24 +0300

    renamed dto, adding Station exception, getStationById

--- src/main/java/org/project/trainticketbookingsystem/service/impl/StationServiceImpl.java
+++ src/main/java/org/project/trainticketbookingsystem/service/impl/StationServiceImpl.java
@@ -1,15 +1,14 @@
 package org.project.trainticketbookingsystem.service.impl;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.project.trainticketbookingsystem.dto.StationDTO;
+import org.project.trainticketbookingsystem.dto.StationDto;
 import org.project.trainticketbookingsystem.entity.StationEntity;
+import org.project.trainticketbookingsystem.exceptions.StationException;
 import org.project.trainticketbookingsystem.mapper.StationMapper;
 import org.project.trainticketbookingsystem.repository.StationRepository;
 import org.project.trainticketbookingsystem.service.StationService;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,18 +18,17 @@ public class StationServiceImpl implements StationService {
     private final StationMapper stationMapper;
 
     @Override
-    public List<StationDTO> getAllStations() {
+    public List<StationDto> getAllStations() {
         List<StationEntity> stations = stationRepository.findAll();
         return stationMapper.toStationDTO(stations);
     }
 
     @Override
-    public StationDTO addStation(StationDTO stationDTO) {
+    public StationDto addStation(StationDto stationDTO) {
         StationEntity stationEntity = stationMapper.toStationEntity(stationDTO);
-        if(isExistsStation(stationEntity))
-            throw new RuntimeException("Station already exists");
+        if (isExistsStation(stationEntity))
+            throw new StationException("Station already exists");
 
-        stationRepository.save(stationEntity);
         return stationMapper.toStationDTO(stationRepository.save(stationEntity));
     }
 
@@ -42,10 +40,16 @@ public class StationServiceImpl implements StationService {
     @Override
     public StationEntity getStationByName(String stationName) {
         StationEntity stationEntity = stationRepository.findByName(stationName);
-        if(stationEntity == null)
-            throw new RuntimeException("Station does not exist");
+        if (stationEntity == null)
+            throw new StationException("Station does not exist");
 
         return stationRepository.findByName(stationName);
+    }
+
+    @Override
+    public StationDto getStationById(Long id) {
+        StationEntity stationEntity = stationRepository.findById(id).orElseThrow(() -> new StationException("Station does not exist"));
+        return stationMapper.toStationDTO(stationEntity);
     }
 
     private boolean isExistsStation(StationEntity stationEntity) {
