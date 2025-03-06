59c139f8b5fb49ef21f0e25e7e05e9e9232bf2d3
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-06 11:34:28 +0300

    mapping

--- src/main/java/org/project/trainticketbookingsystem/mapper/RouteStationTimeMapper.java
+++ src/main/java/org/project/trainticketbookingsystem/mapper/RouteStationTimeMapper.java
@@ -1,11 +1,19 @@
 package org.project.trainticketbookingsystem.mapper;
 
+import java.util.List;
 import org.mapstruct.Mapper;
-import org.project.trainticketbookingsystem.dto.RouteStationTimeDTO;
+import org.mapstruct.Mapping;
+import org.project.trainticketbookingsystem.dto.RouteStationTimeDto;
 import org.project.trainticketbookingsystem.entity.RouteStationTimeEntity;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = StationMapper.class)
 public interface RouteStationTimeMapper {
-    RouteStationTimeEntity toRouteStationTimeEntity(RouteStationTimeDTO routeStationTimeDTO);
-    RouteStationTimeDTO toRouteStationTimeDTO(RouteStationTimeEntity routeStationTimeEntity);
+    RouteStationTimeEntity toRouteStationTimeEntity(RouteStationTimeDto routeStationTimeDTO);
+
+    @Mapping(target = "stationDTO", source = "station")
+    RouteStationTimeDto toRouteStationTimeDTO(RouteStationTimeEntity routeStationTimeEntity);
+
+    List<RouteStationTimeEntity> toRouteStationTimeEntityList(List<RouteStationTimeDto> routeStationTimeDTOList);
 }
+
+
