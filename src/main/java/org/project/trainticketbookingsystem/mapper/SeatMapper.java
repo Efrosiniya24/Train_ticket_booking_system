9eb157158c959d81ca98667cfa1dfd6f15d816c9
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-08 15:49:42 +0300

    toSeatEntity

--- src/main/java/org/project/trainticketbookingsystem/mapper/SeatMapper.java
+++ src/main/java/org/project/trainticketbookingsystem/mapper/SeatMapper.java
@@ -1,13 +1,15 @@
 package org.project.trainticketbookingsystem.mapper;
 
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.project.trainticketbookingsystem.dto.SeatDto;
 import org.project.trainticketbookingsystem.entity.SeatEntity;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface SeatMapper {
     SeatDto toSeatDTO(SeatEntity seatEntity);
+
     List<SeatDto> toSeatDTO(List<SeatEntity> seatEntities);
+
+    List<SeatEntity> toSeatEntity(List<SeatDto> seatDtos);
 }
