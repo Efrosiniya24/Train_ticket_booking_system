3bd98d025177d23a837a4bd73cb6f45e6b6f7c13
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-08 15:52:55 +0300

    getSeatById

--- src/main/java/org/project/trainticketbookingsystem/service/SeatService.java
+++ src/main/java/org/project/trainticketbookingsystem/service/SeatService.java
@@ -1,17 +1,18 @@
 package org.project.trainticketbookingsystem.service;
 
-import org.project.trainticketbookingsystem.dto.BookingDto;
+import java.util.List;
 import org.project.trainticketbookingsystem.dto.SeatDto;
 import org.project.trainticketbookingsystem.entity.CoachEntity;
 import org.project.trainticketbookingsystem.entity.SeatEntity;
 
-import java.util.List;
-
 public interface SeatService {
     List<SeatDto> createSeat(CoachEntity coachEntity, List<SeatDto> seatDtos);
-    List<SeatEntity> getSeatEntityFromBooking(BookingDto bookingDTO);
-    List<Long> getSeatsId(List<SeatEntity> seatEntities);
+
     void deleteSeat(List<SeatEntity> seatEntity);
+
     List<SeatEntity> updateSeats(List<SeatDto> seats);
+
     double getPrice(Long trainId);
+
+    SeatEntity getSeatById(Long seatId);
 }
