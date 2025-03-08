b72e0bb73eba450f136310d5061b5555fc7d0168
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-08 15:47:17 +0300

    to coachDtoList

--- src/main/java/org/project/trainticketbookingsystem/mapper/CoachMapper.java
+++ src/main/java/org/project/trainticketbookingsystem/mapper/CoachMapper.java
@@ -1,5 +1,6 @@
 package org.project.trainticketbookingsystem.mapper;
 
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.project.trainticketbookingsystem.dto.CoachDto;
 import org.project.trainticketbookingsystem.entity.CoachEntity;
@@ -7,4 +8,6 @@ import org.project.trainticketbookingsystem.entity.CoachEntity;
 @Mapper(componentModel = "spring")
 public interface CoachMapper {
     CoachDto toCoachDTO(CoachEntity coachEntity);
+
+    List<CoachDto> toCoachDTO(List<CoachEntity> coachEntities);
 }
