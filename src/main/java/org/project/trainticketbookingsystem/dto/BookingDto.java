d14a416677584364b7c7a68ec8dcadf03e066e04
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-08 15:45:05 +0300

    seatDto

--- src/main/java/org/project/trainticketbookingsystem/dto/BookingDto.java
+++ src/main/java/org/project/trainticketbookingsystem/dto/BookingDto.java
@@ -3,12 +3,11 @@ package org.project.trainticketbookingsystem.dto;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.Data;
-import org.project.trainticketbookingsystem.entity.SeatEntity;
 
 @Data
 public class BookingDto {
     private Long bookingId;
-    private List<SeatEntity> seatsList;
+    private List<SeatDto> seatsList;
     private Long trainId;
     private Long routeId;
     private StationDto departureStation;
