f0ef24f307848fe367b6f67c42b681ca026f4efb
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-08 15:53:14 +0300

    getSeatsWithStatus

---
+++ src/main/java/org/project/trainticketbookingsystem/web/SeatController.java
@@ -0,0 +1,24 @@
+package org.project.trainticketbookingsystem.web;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.project.trainticketbookingsystem.dto.GetSeatStatusDto;
+import org.project.trainticketbookingsystem.dto.SeatStatusDtoResponse;
+import org.project.trainticketbookingsystem.service.BookingSeatService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/train/seats")
+@RequiredArgsConstructor
+public class SeatController {
+    private final BookingSeatService bookingSeatService;
+
+    @GetMapping("/seatWithStatus")
+    public ResponseEntity<List<SeatStatusDtoResponse>> getSeatsWithStatus(@RequestBody GetSeatStatusDto statusDto) {
+        return ResponseEntity.ok(bookingSeatService.getSeatsWithStatusForSegment(statusDto));
+    }
+}
