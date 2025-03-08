0672d37d736b08dc4b037d40c66b66fd40d9e547
Efrosiniya24 <efrosiniya24@gmail.com>
2025-03-08 21:02:37 +0300

    package

--- src/main/java/org/project/trainticketbookingsystem/config/WebConfig.java
+++ src/main/java/org/project/trainticketbookingsystem/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.tourAgency.tourAgencyJava.security;
+package org.project.trainticketbookingsystem.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
